045db32602631d4a2e728cd92ec642f869393c64
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-08-08 15:34:24 -0300

    Criacao dos serviços do funcionarioService.service.ts

---
+++ ControleFuncionarioApp/src/FuncionarioApp/ClientApp/app/funcionarioService.service.ts
@@ -0,0 +1,50 @@
+﻿import { Injectable, Inject } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw'; 
+
+@Injectable()
+export class FuncionarioService {
+    funcionarioAppUrl: string = "";
+
+    constructor(private _http: Http, @Inject('BASE_URL') baseUrl: string) {
+        this.funcionarioAppUrl = baseUrl;
+    }
+
+    // Método responsável para fazer tratamento de error:
+    handleError(error: Response) {
+        console.log(error);
+        return Observable.throw(error);
+    }
+
+    // (GET): Método responsável por Retornar Lista de Cidades:
+    getRetornarListaCidades() {
+        return this._http.get(this.funcionarioAppUrl + 'api/Funcionario/RetornarListaCidades')
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+
+    // (GET) Método responsável por Retornar Todos os Funcionários:
+    getObterTodosFuncionarios() {
+        return this._http.get(this.funcionarioAppUrl + 'api/Funcionario/Index')
+            .map((response: Response) => response.json())
+            .catch(this.handleError);
+    }
+
+    // (GET) Método responsável por Retornar determinado Funcionário por Id:
+    getObterFuncionarioPorId(id: number) {
+        return this._http.get(this.funcionarioAppUrl + 'api/Funcionario/Retornar/' + id)
+            .map((response: Response) => response.json())
+            .catch(this.handleError);
+    }
+
+    // (POST) Método responsável por Salvar/Criar um Novo Funcionário:
+    postAdicionarFuncionario(funcionario) {
+        return this._http.post(this.funcionarioAppUrl + 'api/Funcionario/Adicionar', funcionario)
+            .map((response: Response) => response.json())
+            .catch(this.handleError);
+    }
+}
